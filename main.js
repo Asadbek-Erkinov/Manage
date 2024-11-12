266644330672b258fd5b9af022f5e0778323afa0
Asadbek-Erkinov <asadbekeekinovg777@gmail.com>
2024-11-12 07:56:55 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var elSiteHeader = document.querySelector(".site-header-js");
+var elShowMenuBtn = elSiteHeader.querySelector(".js-hamburg-btn");
+
+elShowMenuBtn.addEventListener("click", function() {
+
+  elSiteHeader.classList.toggle("show-menu");
+})
